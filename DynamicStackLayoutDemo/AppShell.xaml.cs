3708fa5668316dda1a12572e1a05289a466e6184
William Bohrmann <wbohrmann@esri.com>
2023-10-18 12:39:58 -0700

    Add project files.

---
+++ DynamicStackLayoutDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DynamicStackLayoutDemo;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ DynamicStackLayoutDemo/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Android.Hardware.Lights;
+using static Android.Icu.Text.ListFormatter;
+
+namespace DynamicStackLayoutDemo;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        this.BindingContext = new MapViewModel();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
